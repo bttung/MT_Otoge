4e917ea1ad599bc9fb279d8b4cf4cd7f9975aff6
bttung <tungfcb@gmail.com>
2014-08-26 18:07:55 +0900

    Added Transform to the ball!

--- Otoge/Assets/Scripts/Ball.cs
+++ Otoge/Assets/Scripts/Ball.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+    Vector3 initialScale;
+
 	// Use this for initialization
 	void Start () {
-	
+        initialScale = transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (Music.CurrentSection.name.StartsWith("Play")) {
+            UpdateScale();
+        }
 	}
+
+    void UpdateScale() {
+        transform.localScale = new Vector3(Random.Range(0.3f, 3.0f), Random.Range(0.3f, 3.0f), Random.Range(0.3f, 3.0f));
+    }
 }
