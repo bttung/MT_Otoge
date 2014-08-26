62f87db94f4c9f910cf6d720921fa9208c5e83f0
bttung <tungfcb@gmail.com>
2014-08-26 17:09:38 +0900

    Added MusicEngine to my project!

---
+++ Otoge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (Music.CurrentSection.name) {
+        case "Start" :
+            if (Music.IsJustChangedSection("Start")) {
+                Debug.Log("Music Entered Start");
+            }
+            break;
+        case "Clear" :
+            if (Music.IsJustChangedSection("Clear")) {
+                Debug.Log("Music entered Clear");
+            }
+            break;
+        case "GameOver" :
+            if (Music.IsJustChangedSection("GameOver")) {
+                Debug.Log("Music entered GameOver");
+            }
+            break;
+        default: // Play
+            if (Music.IsJustChangedSection("Play")) {
+                Debug.Log("Music entered Play");
+            }
+            if (Music.IsJustChangedSection("Play2")) {
+                Debug.Log("Music entered Play2");
+            }
+            if (Music.IsJustChangedSection("Play3")) {
+                Debug.Log("Music entered Play3");
+            }
+            break;
+	    }
+	}
+}
